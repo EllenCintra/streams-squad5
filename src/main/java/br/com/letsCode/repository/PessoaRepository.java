6657bf23af65920c8f5d1a0e040e0706208f909f
Ellen Cintra Monteiro <ellenc.monteiro@gmail.com>
2022-10-04 10:13:16 -0300

    atualizando a repository

--- src/main/java/br/com/letsCode/repository/PessoaRepository.java
+++ src/main/java/br/com/letsCode/repository/PessoaRepository.java
@@ -12,17 +12,5 @@ import java.util.UUID;
 import java.util.stream.Stream;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, UUID> {
-    Stream<Pessoa> findBySignoAndIdade(Signo signo, Integer idade);
-
-    @Query(value = "select * from Pessoa p where p.idade >= 18", nativeQuery = true)
-    Stream<Pessoa> buscarMaiores18();
-
-    Stream<Pessoa> findByGeracao(Geracao geracao);
-
-    @Query(value = "select avg(p.idade) from Pessoa p", nativeQuery = true)
-    float calcularIdadeMedia();
-
-    @Query(value = "select sum(p.idade) from Pessoa p", nativeQuery = true)
-    float calcularSomaIdades();
 
 }
